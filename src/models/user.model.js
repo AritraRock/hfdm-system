e9d897caa44c6098724ffd6071f8422153c8f37c
Aritra <aritrabts@gmail.com>
2025-01-08 22:35:17 +0530

    update user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from'bcrypt'
 
 const userSchema = new Schema(
     {
@@ -26,6 +28,9 @@ const userSchema = new Schema(
         phone: {
             type: String
         },
+        refreshToken: {
+            type: String,
+        }
     }
 );
 
@@ -59,5 +64,41 @@ const userSchema = new Schema(
 //         }
 //     }
 // };
+
+userSchema.pre("save", async function(next) {
+    if(this.isModified("password")) this.password= await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 export const User = mongoose.model("User", userSchema);
